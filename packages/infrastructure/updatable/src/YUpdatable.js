9c7ef57318275622fb6ad506de42ee2908a2dcbd
hazen <hoyeungw@outlook.com>
2022-05-21 11:59:39 +0800

    add functions to YUpdatable

--- packages/infrastructure/updatable/src/YUpdatable.js
+++ packages/infrastructure/updatable/src/YUpdatable.js
@@ -1,12 +1,25 @@
 export class YUpdatable {
-  head
-  rows
-  constructor({ head, rows }) { this.head = head, this.rows = rows }
-  set(key, column) {}
-  prepend(column) {}
-  append(column) {}
-  shift(column) {}
-  pop(column) {}
-  delete(key) {}
-  grow(from, to, as, at) {}
+  /** @type {Array} */ head
+  /** @type {Array} */ rows
+  constructor({head, rows}) { this.head = head, this.rows = rows }
+  set(y, column) {
+    if (~(y = this.head.indexOf(y))) for (let i = 0, h = this.rows.length; i < h; i++) { this.rows[i][y] = column[i] }
+    return this
+  }
+  delete(y) {
+    if (~(y = this.head.indexOf(y))) { this.head.splice(y, 1), this.rows.map(row => row.splice(y, 1)) }
+    return this
+  }
+  prepend(y, column) { return this.head.unshift(y), this.rows.unshift(column), this }
+  append(y, column) { return this.head.push(y), this.rows.push(column), this }
+  shift() { return [ this.head.unshift(), this.rows.unshift() ] }
+  pop() { return [ this.head.pop(), this.rows.pop() ] }
+  grow(from, to, as, at) {
+    if (~(from = this.head.indexOf(from)) && ~(at = this.head.indexOf(at))) {
+      at++
+      this.head.splice(at, 0, as)
+      this.rows.forEach(row => row.splice(at, 0, to(row[from])))
+    }
+    return this
+  }
 }
