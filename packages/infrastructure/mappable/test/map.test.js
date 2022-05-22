ced5af5b7b355fb58b3f0415a65fd96cab58e710
hazen <hoyeungw@outlook.com>
2022-05-22 12:22:14 +0800

    add test

---
+++ packages/infrastructure/mappable/test/map.test.js
@@ -0,0 +1,26 @@
+import { decoCrostab, logger } from '@spare/logger'
+import { XMappable }           from '../src/XMappable'
+import { YMappable }           from '../src/YMappable'
+
+
+const crostab = {
+  side: [ 'A', 'B', 'C', 'D', 'E' ],
+  head: [ 'a', 'b', 'c', 'd' ],
+  rows: [
+    [ 0, 1, 2, 3 ],
+    [ 0, 1, 2, 3 ],
+    [ 0, 1, 2, 3 ],
+    [ 0, 1, 2, 3 ],
+    [ 0, 1, 2, 3 ],
+  ]
+}
+
+const xMappable = new XMappable(crostab)
+const yMappable = new YMappable(crostab)
+
+xMappable
+  .mutate([ 'B', 'C' ], x => x +1)
+  .mutate([ 'B', 'C' ], x => x * 10)
+yMappable.mutate([ 'b', 'c' ], x => x * 10)
+
+crostab |> decoCrostab |> logger
