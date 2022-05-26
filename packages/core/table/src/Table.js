f55cb2be0c7f237379c3999287fb1cf6e2fdd581
hazen <hoyeungw@outlook.com>
2022-05-26 11:27:04 +0800

    add Table static builder

--- packages/core/table/src/Table.js
+++ packages/core/table/src/Table.js
@@ -1,9 +1,15 @@
 import { Headward } from '@analyz/crostab'
+import { shallow }  from '@vect/matrix-init'
 
 export class Table extends Headward {
   title
   constructor(table) {
     super(table)
     this.title = table?.title
+  }
+  static from(o = {}) { return new Table(o) }
+  slice() {
+    let {head, rows, title} = this
+    return new Table({head: head.slice(), rows: shallow(rows), title})
   }
 }
