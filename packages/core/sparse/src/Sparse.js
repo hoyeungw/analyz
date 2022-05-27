66bcec5e71745fc364bb06c8f55bc2f4b4662e6b
hoyeungw <hoyeungw@outlook.com>
2022-05-27 15:51:11 +0800

    wrap field 'data' instead of assign value directly on Sparse instance

--- packages/core/sparse/src/Sparse.js
+++ packages/core/sparse/src/Sparse.js
@@ -6,9 +6,12 @@ import { indexed, indexedTo } from './infrastructure/indexed'
 // public field is not allowed to be assigned to Sparse instance
 
 export class Sparse {
+  data = {}
   #init = null
   #base = null
-  constructor(el) { el instanceof Function ? (this.#init = el) : (this.#base = el) }
+  constructor(el) {
+    el instanceof Function ? (this.#init = el) : (this.#base = el)
+  }
   static build(el) { return new Sparse(el) }
   static from(nested) {
     const sparse = new Sparse()
@@ -22,33 +25,33 @@ export class Sparse {
   }
 
   [Symbol.iterator]() { return this.indexed() }
-  clear() { for (let x in this) delete this[x] }
-  get #zero() { return this.#init?.call(this) ?? this.#base }
+  clear() { for (let x in this.data) delete this.data[x] }
+  get zero() { return this.#init?.call(this) ?? this.#base }
   cell(x, y) {
-    const row = this[x]
+    const row = this.data[x]
     return row ? row[y] : null
   }
   cellOrInit(x, y) {
-    const row = this[x] ?? (this[x] = {})
-    return row[y] ?? (row[y] = this.#zero)
+    const row = this.data[x] ?? (this.data[x] = {})
+    return row[y] ?? (row[y] = this.zero)
   }
-  row(x) { return this[x] ?? (this[x] = {}) }
+  row(x) { return this.data[x] ?? (this.data[x] = {}) }
   rowOn(x, y) {
-    const row = this[x] ?? (this[x] = {})
-    if (!(y in row)) row[y] = this.#zero
+    const row = this.data[x] ?? (this.data[x] = {})
+    if (!(y in row)) row[y] = this.zero
     return row
   }
-  update(x, y, v) { (this[x] ?? (this[x] = {}))[y] = v }
+  update(x, y, v) { (this.data[x] ?? (this.data[x] = {}))[y] = v }
   collect(iter) {
     for (let [ x, y, v ] of iter) this.update(x, y, v)
     return this
   }
-  * indexed(by, to) { yield* indexed(this, by, to) }
-  * indexedTo(to) { yield* indexedTo(this, to) }
-  get side() { return Object.keys(this) }
+  * indexed(by, to) { yield* indexed(this.data, by, to) }
+  * indexedTo(to) { yield* indexedTo(this.data, to) }
+  get side() { return Object.keys(this.data) }
   get head() {
     const vec = []
-    for (let x in this) for (let y in this[x]) if (!~vec.indexOf(y)) vec.push(y)
+    for (let x in this.data) for (let y in this.data[x]) if (!~vec.indexOf(y)) vec.push(y)
     return vec
   }
   crostab(to, nu) {
