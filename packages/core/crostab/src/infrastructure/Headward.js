8f09841b30e4e1bf134cd9d24517558842f2af42
hoyeungw <hoyeungw@outlook.com>
2022-05-27 09:01:13 +0800

    restructure files

--- packages/core/crostab/src/infrastructure/Headward.js
+++ packages/core/crostab/src/infrastructure/Headward.js
@@ -1,15 +1,23 @@
+import { ProxyFab }    from '@analyz/indexable'
 import { YMappable }   from '@analyz/mappable'
 import { YSelectable } from '@analyz/selectable'
 import { YUpdatable }  from '@analyz/updatable'
 
-export class Headward {
+export class Headward  {
   head
   rows
-  constructor({head, rows}) { this.head = head, this.rows = rows }
-  // mapKeys(fn) { return YMappable.prototype.mapKeys.call(this, fn) }
-  // map(keys, fn) { return YMappable.prototype.map.call(this, keys, fn) }
+  constructor({ head, rows }) { this.head = head, this.rows = rows }
+
+  get at() { return this._hdi ?? (this._hdi = ProxyFab.headwardIndexer(this)) }
+
   mutateKeys(fn) { return YMappable.prototype.mutateKeys.call(this, fn) }
   mutate(keys, fn) { return YMappable.prototype.mutate.call(this, keys, fn) }
+  * indexed(by, to) { yield* YMappable.prototype.indexed.call(this, by, to) }
+  * entryIndexed(kv, by, to) { yield* YMappable.prototype.entryIndexed.call(this, kv, by, to) }
+  * tripletIndexed(xyz, by, to) { yield* YMappable.prototype.tripletIndexed.call(this, xyz, by, to) }
+  * indexedTo(to) { yield* YMappable.prototype.indexedTo.call(this, to) }
+  * entryIndexedTo(kv, to) { yield* YMappable.prototype.entryIndexedTo.call(this, kv, to) }
+  * tripletIndexedTo(xyz, to) { yield* YMappable.prototype.tripletIndexedTo.call(this, xyz, to) }
 
   select(keys) { return YSelectable.prototype.select.call(this, keys) }
   filter(x, by) { return YSelectable.prototype.filter.call(this, x, by) }
--- packages/core/crostab/src/infrastructure/Sideward.js
+++ packages/core/crostab/src/infrastructure/Sideward.js
@@ -1,3 +1,4 @@
+import { ProxyFab }    from '@analyz/indexable'
 import { XMappable }   from '@analyz/mappable'
 import { XSelectable } from '@analyz/selectable'
 import { XUpdatable }  from '@analyz/updatable'
@@ -5,11 +6,20 @@ import { XUpdatable }  from '@analyz/updatable'
 export class Sideward {
   side
   rows
-  constructor({side, rows}) { this.side = side, this.rows = rows }
-  // mapKeys(fn) { return XMappable.prototype.mapKeys.call(this, fn) }
-  // map(keys, fn) { return XMappable.prototype.map.call(this, keys, fn) }
+  constructor({ side, rows }) {
+    this.side = side, this.rows = rows
+  }
+
+  get at() { return this._sdi ?? (this._sdi = ProxyFab.sidewardIndexer(this)) }
+
   mutateKeys(fn) { return XMappable.prototype.mutateKeys.call(this, fn) }
   mutate(keys, fn) { return XMappable.prototype.mutate.call(this, keys, fn) }
+  * indexed(by, to) { yield* XMappable.prototype.indexed.call(this, by, to) }
+  * entryIndexed(kv, by, to) { yield* XMappable.prototype.entryIndexed.call(this, kv, by, to) }
+  * tripletIndexed(xyz, by, to) { yield* XMappable.prototype.tripletIndexed.call(this, xyz, by, to) }
+  * indexedTo(to) { yield* XMappable.prototype.indexedTo.call(this, to) }
+  * entryIndexedTo(kv, to) { yield* XMappable.prototype.entryIndexedTo.call(this, kv, to) }
+  * tripletIndexedTo(xyz, to) { yield* XMappable.prototype.tripletIndexedTo.call(this, xyz, to) }
 
   select(keys) { return XSelectable.prototype.select.call(this, keys) }
   filter(x, by) { return XSelectable.prototype.filter.call(this, x, by) }
