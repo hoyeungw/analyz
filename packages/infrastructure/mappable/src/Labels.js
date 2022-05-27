f85abcaecd1ab7a1af995d2c09e4f1ce3c059927
hoyeungw <hoyeungw@outlook.com>
2022-05-27 10:41:31 +0800

    fix bugs for Labels methods

--- packages/infrastructure/mappable/src/Labels.js
+++ packages/infrastructure/mappable/src/Labels.js
@@ -1,8 +1,21 @@
 export class Labels extends Array {
   constructor(size) { super(size) }
-  indexesOf(keys) { return keys.map(key => this.indexOf(key)) }
-  indexAt(key) { return Array.isArray(key) ? { at: this.indexOf(key[0]), as: key[1] } : { at: this.indexOf(key), as: key } }
-  indexesAt(keys) { return keys.map(Labels.prototype.indexAt.bind(this)) }
+  indexesOf(keys) {
+    let inds = [], i
+    for (let key of keys) if (~(i = this.indexOf(key))) inds.push(i)
+    return inds
+  }
+  indexAt(key) {
+    let at, from, as
+    return Array.isArray(key)
+      ? ([ from, as ] = key) && ~(at = this.indexOf(from)) ? { at, as } : null
+      : (from = key, as = key) && ~(at = this.indexOf(from)) ? { at, as } : null
+  }
+  indexesAt(keys) {
+    let inds = [], info, { indexAt } = Labels.prototype
+    for (let key of keys) if ((info = indexAt.call(this, key))) inds.push(info)
+    return inds
+  }
 }
 
 export const { indexesOf, indexesAt } = Labels.prototype
