059988a48285603beef2e990fff20a15c9b88261
hoyeungw <hoyeungw@outlook.com>
2022-06-06 10:38:34 +0800

    rename copyFrom to acquire

--- packages/core/samples/src/Samples.js
+++ packages/core/samples/src/Samples.js
@@ -1,18 +1,28 @@
-import { select } from '@vect/object-select'
+import { select, values } from '@vect/object-select'
+import { Table }          from '@analyz/table'
+import { keys }           from '@vect/object-index'
+import { mapper }         from '@vect/vector-mapper'
+import { Stat }           from '@analyz/crostab'
+import { tripletIndexed } from '@vect/matrix-mapper'
 
 export class Samples extends Array {
-  title
+  title = null
   constructor(hi) { super(hi) }
   static build(hi) { return new Samples(hi) }
-  copyFrom(another) {
-    for (let i = 0, h = another.length; i < h; i++) {this[i] = another}
+  static of(...samples) { return (new Samples(samples?.length)).acquire(samples) }
+  static from(samples) { return (new Samples(samples?.length)).acquire(samples)}
+
+  get head() { return this.length ? keys(this[0]) : [] }
+
+  acquire(samples) {
+    for (let i = 0, h = samples.length; i < h; i++) { this[i] = samples[i] }
     return this
   }
   collect(iter) {
     for (let sample of iter) { this.push(sample) }
     return this
   }
-  get head() { return this.length ? Object.keys(this[0]) : [] }
+
   select(keys) { return this.copy(this.map(select.bind(keys))) }
   copy(samples) { return Samples.build((samples = samples ?? this).length).copyFrom(samples) }
   table() { return {head: this.head, rows: this.map(Object.values), title: this.title} }
